c80bf2b33ffc3e12ff823192b259a588282c3e07
DITTHAPHONG JARATCHAIROT <63010354@KMITL.AC.TH>
2020-08-30 21:35:07 +0700

    Add project files.

---
+++ กำแพงเมืองไทย/Source.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+long int lend, brick, gcd;
+long int Wall, h[100001], x, hcopy[100001];
+
+int gcdfunction(long int a, long int b)
+{
+	while (b != 0)
+	{
+		lend = b;
+		b = a % b;
+		a = lend;
+	}
+	gcd = a;
+	return gcd;
+}
+
+int main()
+{
+	cin >> Wall;
+	cin >> h[0];
+	gcd = h[0];
+
+	for (int i = 1; i < Wall; i++)
+	{
+		cin >> h[i];
+		brick += h[i];
+	}
+	brick += h[0];
+
+	for (int i = 1; i < Wall; i++)
+	{
+		gcd = gcdfunction(gcd, h[i]);
+	}
+
+	cout << brick / gcd;
+
+}
